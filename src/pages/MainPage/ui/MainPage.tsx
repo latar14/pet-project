d815d8400c40c56ed289f1bc5dc9109707cc7385
Ashwash <latar14@bk.ru>
2023-07-13 19:59:44 +0300

    input shared component

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,19 @@
 import { Counter } from 'entities/Counter';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation();
+    const [value, setValue] = useState('');
+    const onChange = (val: string) => {
+        setValue(val);
+    };
 
     return (
         <div>
             {t('Главная страница')}
-            <Counter />
+            <Input value={value} onChange={onChange} placeholder={t('Введите текст')} />
         </div>
     );
 };
