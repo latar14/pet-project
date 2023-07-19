80ad920b61b3ece487ee6e77050104763a877266
Ashwash <latar14@bk.ru>
2023-07-19 13:05:35 +0300

    fix storedecorator problems

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,21 +1,26 @@
 import { Provider } from 'react-redux';
 import { createReduxStore } from 'app/providers/StoreProvider/config/store';
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { ReactNode } from 'react';
 
 interface StoreProviderProps {
     children: ReactNode;
     initialState?: DeepPartial<StateSchema>;
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
     const {
         children,
         initialState,
+        asyncReducers,
     } = props;
 
-    const store = createReduxStore(initialState as StateSchema);
+    const store = createReduxStore(
+initialState as StateSchema,
+        asyncReducers as ReducersMapObject<StateSchema>,
+    );
 
     return (
         <Provider store={store}>
